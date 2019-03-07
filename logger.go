1980da2184b54737a7ee42785c7cfd19f5f0857c
kaizhangkai <longbowk@gmail.com>
2019-03-08 00:28:00 +0800

    update

--- logger.go
+++ logger.go
@@ -3,7 +3,7 @@ package httpclient
 import "github.com/k81/log"
 
 // logger is the default logger used
-var logger *log.Logger = log.DefaultLogger
+var logger *log.Logger = log.GetLogger()
 
 func SetLogger(l *log.Logger) {
 	logger = l

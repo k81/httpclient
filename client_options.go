d8af5b88f3eefb7b84f20d85e4481403db8e88ca
kaizhangkai <kaizhangkai@didichuxing.com>
2019-03-24 13:35:47 +0800

    update

--- client_options.go
+++ client_options.go
@@ -25,7 +25,7 @@ func Timeout(timeout time.Duration) ClientOption {
 // SetTransport set the transport of client
 func SetTransport(transport http.RoundTripper) ClientOption {
 	return func(client *Client) {
-		client.Transport = NewLogTransport(client.ctx, transport)
+		client.Transport = transport
 	}
 }
 
